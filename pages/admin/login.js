620222b791c0bd318ad31ec27f6de5827f098996
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-08-05 19:18:49 +0530

    Update

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,8 +1,37 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+
+import jwt from 'jsonwebtoken'
 
 export default function Login(){
-    
+    const [username, setusername] = useState('')
+    const [password, setpassword] = useState('')
+    const [message, setmessage] = useState("You are not  LogIn!")
+
+    const onsubmit = async(e) =>{
+        e.preventDefault();
+        const res = await fetch('http://localhost:3000/api/admin/login',{
+            method:'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({username,password})
+        }).then((t)=>t.json())
+
+        //const token = res.token
+        if(token)
+        {
+            //const json = jwt.decode(token)
+            console.log(password)
+            setmessage(`welcome ${username} and ${password}! } `)
+        }
+        else
+        {
+            setmessage('Something Went Wrong!')
+        }
+
+    }
     return(
         <>
             <style global jsx>{`html, body,div#__next{ height: 100%; }`}</style> 
@@ -10,27 +39,28 @@ export default function Login(){
                 <div className='container login-container'>
                     <div className='login-div'>
                        <h2 className='login-title'>Automation Tool Login</h2>
-                            <form method='POST' className="login-main" >
+                        <h2>{message}</h2>
+                            <form method='POST' className="login-main"  >
                             <div id='personal-account'>
                                 <div className="form-group"  >
-                                        <label htmlFor="ba-num"  className='form-label'>Email</label>
-                                        <input type="text" id="name" name="name" className='form-control login-input' />
+                                    <label htmlFor="ba-num"  className='form-label'>Email</label>
+                                    <input type="text" name="username" value={username} onChange={e=>setusername(e.target.value)} className='form-control login-input' />
                                 </div>
                                 <div className="form-group">
-                                        <label htmlFor="pwd" className='form-label label'>Password</label>
-                                        <input type="password" id="pwd"  name="pwd" className='form-control login-input' />
+                                    <label htmlFor="pwd" className='form-label label'>Password</label>
+                                    <input type="password"  name="password" value={password} onChange={e=>setpassword(e.target.value)}  className='form-control login-input' />
                                 </div>  
                                 <div className='login-head'>
                                     <div className='login-col'>
                                         <input type="checkbox" /><label className="check" htmlFor="">Remember me</label>
                                     </div>
-                                    <div className='login-two'>
-                                        <Link href='#'><a><span className='login-text-login'>Forgot Password?</span></a></Link>
-                                    </div>
+                                    {/*<div className='login-two'>
+                                        <Link href='/'><a><span className='login-text-login'>Forgot Password?</span></a></Link>
+                                    </div>*/}
                                 </div> 
                                 
                                 <div className="login-btn">
-                                    <button type="submit" className="login-create-acc-btn">Login</button>  
+                                    <button type="submit" onClick={onsubmit} className="login-create-acc-btn">Login</button>  
                                 </div>         
                             </div>
                         </form>
--- pages/api/admin/login.js
+++ pages/api/admin/login.js
@@ -1,8 +1,21 @@
-import jwt from 'jsonwebtoken'
-const KEY = 'asdfghjklzxcvbnmjtttdgsnrgjgnamffadwrqwww'
+//import jwt from 'jsonwebtoken'
+//const KEY = 'asdfghjklzxcvbnmjtttdgsnrgjgnamffadwrqwww'
+
+import nc from "next-connect";
+import { getData } from '../../../controller/admin/login'
+
+const handler2=nc();
+handler2.get(getData);
+export default handler2;
 
 
-async function Login(req,res){
+
+
+
+
+/*async function Login(req,res){
+        
+
     console.log(req.body);
 
     if(!req.body){
@@ -10,17 +23,24 @@ async function Login(req,res){
         res.end('Error')
         return
     }
+    
     const { username, password } = req.body
+    
+    console.log(username)
+    console.log(password)
+
 
     res.json({
         token: jwt.sign(
             {
                 username,
-                admin:username==='admin' && password==='admin'
+                password,
+                //admin:username==='user' && password==='User@1234'
+                admin:username===username && password===password
             },
             KEY
         )
     })
 }
 
-export default Login
+export default {Login,handler2}*/
