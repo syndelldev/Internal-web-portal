3f36897dc0296479d052475d10492c19dbcc6ed4
ashvinisyndell <ashvini.syndell@gmail.com>
2022-08-18 14:13:05 +0530

    Update

--- routes.js
+++ routes.js
@@ -49,6 +49,14 @@ const dashboardRoutes = [
     layout: "/admin",
   },
   {
+    path: "/project_module",
+    name: "Create Project",
+    rtlName: "قائمة الجدول",
+    icon: "person",
+
+    layout: "/admin",
+  },
+  {
     path: "/user-rights",
     name: "User Rights",
     rtlName: "قائمة الجدول",
