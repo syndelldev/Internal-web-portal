1135cd6fa85327573d7dd5fdddc5c5988de2eeb2
priyankap-bit <>
2022-08-26 19:19:27 +0530

    Updated sidebar js

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -100,7 +100,7 @@ export default function Sidebar(props) {
   );
   return (
     <div>
-      {/*<Hidden mdUp implementation="css">
+      <Hidden mdUp implementation="css">
         <Drawer
           variant="temporary"
           anchor={props.rtlActive ? "left" : "right"}
@@ -127,7 +127,7 @@ export default function Sidebar(props) {
             />
           ) : null}
         </Drawer>
-          </Hidden>*/}
+          </Hidden>
       <Hidden smDown implementation="css">
         <Drawer
           anchor={props.rtlActive ? "right" : "left"}
