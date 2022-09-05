1e6f512e1dfe4e632dc2a78c8d8cf95004ebe22a
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-09-05 11:57:56 +0530

    Update

--- pages/admin/user-rights/index.js
+++ pages/admin/user-rights/index.js
@@ -44,7 +44,7 @@ export async function getServerSideProps(context){
     //const id = context.params.userdetailid;
     const res = await fetch(`${server}/api/rights/`)
     const data = await res.json()
-    //console.log(data)
+    console.log(data)
 
     return { props: {data}, }
 }
@@ -166,7 +166,7 @@ function UserRights({data}){
                                                     <TableCell>{rights.page_id}-{rights.role}</TableCell>
                                                     <TableCell>
                                                         <input type="checkbox"  onChange={()=>setchecklist(!checklist)} onClick={rightlist} />{rights.user_list}
-                                                        <p> {checklist ? '1' : '0'}</p> 
+                                                        {/*<p> {checklist ? '1' : '0'} </p> */}
                                                     </TableCell>
                                                     <TableCell>
                                                         <input type="checkbox" onChange={()=>setcheckaddlist(!checkaddlist)} onClick={()=>addlist()} />{rights.add_user}
