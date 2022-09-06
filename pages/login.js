0d7122c3d4700d72bb5b81446139fbb8a6c9c204
riya patel <riya.syndell@gmail.com>
2022-09-06 12:35:40 +0530

    css changes

--- pages/login.js
+++ pages/login.js
@@ -8,7 +8,6 @@ import { server } from 'config';
 import { useCookies } from 'react-cookie';
 
 import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 export default function home()
 {
