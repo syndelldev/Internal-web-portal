dfa1a32c59a332f6208ccad8da1225733741c5b6
riya patel <riya.syndell@gmail.com>
2022-09-09 18:00:14 +0530

    site

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://internal-web.vercel.app';
+export const server = dev ? 'http://localhost:3000' : 'automationtool.com';
