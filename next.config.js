d2b7a9dd3de5f1e8f562d188ee071e3f0be37f74
riya patel <riya.syndell@gmail.com>
2022-09-09 12:07:53 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
+/** @type {import('next').NextConfig} */
+
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
 const webpack = require("webpack");
 const path = require("path");
 
-module.exports = withPlugins([ [withImages] ], {
-  webpack(config, options) {
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     domains: ['assets.vercel.com'],
+//     formats: ['image/avif', 'image/webp'],
+//   },
+// }
+
+module.exports = withPlugins([ [withImages] , {
+  webpack: (config, options) => {
     config.resolve.modules.push(path.resolve("./"));
     return config;
   },
-});
+  images: {
+    disableStaticImages: true,
+  },
+}]);
