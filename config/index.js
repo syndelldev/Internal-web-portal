0f6a94d8fa5ebbed75e29a5b3ed2a4a6bc162318
riya patel <riya.syndell@gmail.com>
2022-09-09 18:24:19 +0530

    site name changes

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://internal-web-portal.com';
+export const server = dev ? 'http://localhost:3000' : 'https://internal-web-portal.vercel.app/';
