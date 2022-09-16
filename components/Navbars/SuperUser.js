08456062448649df329e63cde3cf62b665cc48d4
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-09-16 17:51:22 +0530

    Update

---
+++ components/Navbars/SuperUser.js
@@ -0,0 +1,286 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Hidden from "@material-ui/core/Hidden";
+import Poppers from "@material-ui/core/Popper";
+import Divider from "@material-ui/core/Divider";
+// @material-ui/icons
+import Person from "@material-ui/icons/Person";
+import Notifications from "@material-ui/icons/Notifications";
+import Dashboard from "@material-ui/icons/Dashboard";
+import Search from "@material-ui/icons/Search";
+// core components
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+import useWindowSize from "components/Hooks/useWindowSize.js";
+
+import axios from "axios";
+import styles from "assets/jss/nextjs-material-dashboard/components/headerLinksStyle.js";
+import { server } from 'config';
+import { useCookies } from 'react-cookie';
+
+//axios.defaults.withCredentials=true;
+
+export default function AdminNavbarLinks({useravtar}) {
+
+  
+  // useEffect(async() => {
+  //   const res = await fetch(`${server}/api/admin/${cookies.Id}`)
+  //   const useravtar=await res.json()
+  //   console.log(useravtar)
+  // }, []);
+  // console.log(useravtar)
+
+  // const [users, setusers] = useState([])
+  // useEffect(async()=>{
+  //   axios.get(`${server}/api/admin/${cookies.Id}`, {withCredentials: true})
+  //     .then((res)=>{
+  //       setusers(res.data)
+  //       //console.log(res.data)
+  //     })    
+  // },[])
+  // console.log(users)
+
+  const router = useRouter();
+  const [cookies, setCookie, removeCookie ] = useCookies();
+  //console.log(cookies.Id)
+  const logoutfunc = () => {
+    removeCookie('name', { path:'/' } );
+    removeCookie('Email', { path:'/' } );
+    removeCookie('Mobile_num', { path:'/' } );
+    removeCookie('DOB', { path:'/' } );
+    removeCookie('Department', { path:'/' } );
+    removeCookie('Position', { path:'/' } );
+    removeCookie('Role', { path:'/' } );
+    removeCookie('Avtar', { path:'/' } );
+    removeCookie('Id', { path:'/' } );
+    removeCookie('Role_id', { path:'/' } );
+
+    router.push("/login");
+  }
+ 
+
+
+  const size = useWindowSize();
+  const useStyles = makeStyles(styles);
+  const classes = useStyles();
+  const [openNotification, setOpenNotification] = React.useState(null);
+  const [openProfile, setOpenProfile] = React.useState(null);
+  const handleClickNotification = (event) => {
+    if (openNotification && openNotification.contains(event.target)) {
+      setOpenNotification(null);
+    } else {
+      setOpenNotification(event.currentTarget);
+    }
+  };
+  const handleCloseNotification = () => {
+    setOpenNotification(null);
+  };
+  const handleClickProfile = (event) => {
+    if (openProfile && openProfile.contains(event.target)) {
+      setOpenProfile(null);
+    } else {
+      setOpenProfile(event.currentTarget);
+    }
+  };
+  const handleCloseProfile = () => {
+    setOpenProfile(null);
+  };
+  return (
+    <div>
+      {/*<div className={classes.searchWrapper}>
+        <CustomInput
+          formControlProps={{
+            className: classes.margin + " " + classes.search,
+          }}
+          inputProps={{
+            placeholder: "Search",
+            inputProps: {
+              "aria-label": "Search",
+            },
+          }}
+        />
+        <Button color="white" aria-label="edit" justIcon round>
+          <Search />
+        </Button>
+        </div>*/}
+        
+      {/*<Button
+        color={size.width > 959 ? "transparent" : "white"}
+        justIcon={size.width > 959}
+        simple={!(size.width > 959)}
+        aria-label="Dashboard"
+        className={classes.buttonLink}
+      >
+        <Dashboard className={classes.icons} />
+        <Hidden mdUp implementation="css">
+          <p className={classes.linkText}>Dashboard</p>
+        </Hidden>
+      </Button>*/}
+      {/*<div className={classes.manager}>
+        <Button
+          color={size.width > 959 ? "transparent" : "white"}
+          justIcon={size.width > 959}
+          simple={!(size.width > 959)}
+          aria-owns={openNotification ? "notification-menu-list-grow" : null}
+          aria-haspopup="true"
+          onClick={handleClickNotification}
+          className={classes.buttonLink}
+        >
+          <Notifications className={classes.icons} />
+          <span className={classes.notifications}>5</span>
+          <Hidden mdUp implementation="css">
+            <p onClick={handleCloseNotification} className={classes.linkText}>
+              Notification
+            </p>
+          </Hidden>
+        </Button>
+        {/*<Poppers
+          open={Boolean(openNotification)}
+          anchorEl={openNotification}
+          transition
+          disablePortal
+          className={
+            classNames({ [classes.popperClose]: !openNotification }) +
+            " " +
+            classes.popperNav
+          }
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              id="notification-menu-list-grow"
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleCloseNotification}>
+                  <MenuList role="menu">
+                    <MenuItem
+                      onClick={handleCloseNotification}
+                      className={classes.dropdownItem}
+                    >
+                      Mike John responded to your email
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleCloseNotification}
+                      className={classes.dropdownItem}
+                    >
+                      You have 5 new tasks
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleCloseNotification}
+                      className={classes.dropdownItem}
+                    >
+                      You{"'"}re now friend with Andrew
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleCloseNotification}
+                      className={classes.dropdownItem}
+                    >
+                      Another Notification
+                    </MenuItem>
+                    <MenuItem
+                      onClick={handleCloseNotification}
+                      className={classes.dropdownItem}
+                    >
+                      Another One
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+            </Poppers>
+        </div>*/}
+      <div className={classes.manager}>
+      
+        <Button
+          color={size.width > 959 ? "transparent" : "white"}
+          justIcon={size.width > 959}
+          simple={!(size.width > 959)}
+          aria-owns={openProfile ? "profile-menu-list-grow" : null}
+          aria-haspopup="true"
+          onClick={handleClickProfile}
+          className={classes.buttonLink}  
+        >
+          <Person className={classes.icons} />
+          {/*{users.map((avtar)=>{
+          return(
+            <div key={avtar.id}>
+              <p>{avtar.avtar}</p>
+              <img src={`${server}/avtar.png`} width={40} height={40} className={classes.icons}/>
+            </div>
+          )
+        })}*/}
+          <Hidden mdUp implementation="css">
+            <p className={classes.linkText}>Profile</p>
+          </Hidden>
+        </Button>
+        
+        {/*<input 
+            type='file' 
+            accept="/image/*"
+            onChange={(e)=>{
+              const file = e.target.files[0];
+              if(file && file.type.substring(0,5)==="image"){
+                setavtar(file);
+              }
+              else{
+                setavtar(null)
+              }
+            }}
+          />*/}
+        <Poppers
+          open={Boolean(openProfile)}
+          anchorEl={openProfile}
+          transition
+          disablePortal
+          className={
+            classNames({ [classes.popperClose]: !openProfile }) +
+            " " +
+            classes.popperNav
+          }
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              id="profile-menu-list-grow"
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleCloseProfile}>
+                  <MenuList role="menu" className="logout">
+                    {/*<MenuItem onClick={handleCloseProfile} className={classes.dropdownItem}>
+                      <a href="/user/userprofile">Profile({cookies.name})</a>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseProfile} className={classes.dropdownItem}>
+                      <a href="#">Settings</a>
+                    </MenuItem>*/}
+                    <Divider light />
+                    <MenuItem /*onClick={handleCloseProfile}*/ onClick={logoutfunc} className={classes.dropdownItem}>
+                      <a href="#">Logout</a>
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Poppers>
+      
+      </div>
+    </div>
+  );
+}
--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,43 +1,22 @@
 import React, { useEffect, useState } from "react";
-//import Cookies from 'js-cookie';
-// react plugin for creating charts
-import ChartistGraph from "react-chartist";
-// @material-ui/core
-import { makeStyles } from "@material-ui/core/styles";
-import Icon from "@material-ui/core/Icon";
-import { GiSandsOfTime } from "react-icons/gi";
-import { FiEdit } from 'react-icons/fi'
-import { FaEye } from 'react-icons/fa'
-import { MdDelete } from 'react-icons/md'
-import { RiAddCircleFill } from 'react-icons/ri'
-
 // layout for this page
+
 import User from "layouts/User.js";
+
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
-import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import Table from "@material-ui/core/Table";
-import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
 import Popup from "reactjs-popup";
 import axios from "axios";
 import { server } from 'config';
 
-import {
-  dailySalesChart,
-  emailsSubscriptionChart,
-  completedTasksChart,
-} from "variables/charts.js";
-
+import { makeStyles } from "@material-ui/core/styles";
 import styles from "assets/jss/nextjs-material-dashboard/views/dashboardStyle.js";
 import { useCookies } from 'react-cookie';
 import { Button } from "@material-ui/core";
