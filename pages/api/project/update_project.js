6a3b13c9bc1929852acc7c483f97ed67158f623b
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-09-20 10:42:19 +0530

    Update

--- pages/api/project/update_project.js
+++ pages/api/project/update_project.js
@@ -17,22 +17,29 @@ async function updateProject(req,res){
             var members = req.body.project_person;
             console.log(members)
             
-            const allSelectedUser = [];
-            for(var i=0; i<members.length; i++){
-                allSelectedUser.push(members[i].value);
-                console.log(allSelectedUser);
-            }
+            // const allSelectedUser = [];
+            // for(var i=0; i<req.body.project_person.length; i++){
+            //     allSelectedUser.push(req.body.project_person[i].value);
+            //     console.log(allSelectedUser);
+            // }
             
  
 
             var addUserQuery = await executeQuery("Update `tbl_project` set `project_title` = ?, `project_description` = ?, `project_language` = ?, `project_comment` = ?, `project_priority` = ?, `project_start` = ?, `project_deadline` = ? , `project_person`= ? where `project_id` = ?",
-            [req.body.project_title, req.body.project_description, req.body.project_language , req.body.project_comment , req.body.project_priority , req.body.project_start , req.body.project_deadline , `${req.body.project_person}` , req.body.project_id ] );
+            [req.body.project_title, req.body.project_description, req.body.project_language , req.body.project_comment , req.body.project_priority , req.body.project_start , req.body.project_deadline , `${members}` , req.body.project_id ] );
 
+            const userid = [];
+            for(var i=0; i<members.length; i++){
+                var addUserQuery =  await executeQuery("SELECT id FROM `tbl_user` where username =? ",[`${members[i]}`])
+                var memberId = addUserQuery[0].id
+                userid.push(memberId);
+                // console.log(userid);
+            }
+            console.log([`${userid}`]);
 
-            // for(var i=0; i<req.body.project_person.length; i++){
-            //     var addUserQuery =  await executeQuery("SELECT id FROM `tbl_user` where username =? ",[`${req.body.project_person}`])
-            //     console.log(addUserQuery);
-            // }
+            var addUserQuery =  await executeQuery(" UPDATE `tbl_project_rights` SET `user_id`=? WHERE `project_id` = ? ",[`${userid}`, req.body.project_id])
+            console.log(addUserQuery);
+
 
             res.status(200).json(addUserQuery);
             // console.log(addUserQuery);
