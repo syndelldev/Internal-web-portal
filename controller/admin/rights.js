f9d79d49bfacd0d3f8bd46f6d34c97d6df120d18
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-09-21 12:39:16 +0530

    Update

--- controller/admin/rights.js
+++ controller/admin/rights.js
@@ -27,8 +27,8 @@ const ModuleById = async (req,res) => {
 
     
     try{
-        let rightsId=await executeQuery(` SELECT * FROM tbl_project `, [] );/*WHERE tbl_rights.user_id=${id}*/
-        // let rightsId=await executeQuery(" SELECT * FROM tbl_project_rights RIGHT JOIN tbl_rights ON tbl_project_rights.project_id=tbl_rights.project_id INNER JOIN tbl_project ON tbl_project.project_id=tbl_rights.project_id WHERE tbl_project_rights.user_id LIKE ? AND tbl_rights.user_id=?", [`%${req.body.userid}%`,req.body.userid] );
+        // let rightsId=await executeQuery(` SELECT * FROM tbl_project `, [] );/*WHERE tbl_rights.user_id=${id}*/
+        let rightsId=await executeQuery(" SELECT * FROM tbl_project_rights RIGHT JOIN tbl_rights ON tbl_project_rights.project_id=tbl_rights.project_id INNER JOIN tbl_project ON tbl_project.project_id=tbl_rights.project_id WHERE tbl_project_rights.user_id LIKE ? AND tbl_rights.user_id=?", [`%${req.body.userid}%`,req.body.userid] );
         res.status(200).json(rightsId);
         //console.log(rightsId)
     }
