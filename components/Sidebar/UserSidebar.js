6f700a9c3dc31d3936b691593f8b436400124450
kalpesh-syndell <113024711+kalpesh-syndell@users.noreply.github.com>
2022-09-26 15:07:43 +0530

    Changes
    UserSiderbar.js
    project.js
    usertask.js
    global.css

--- components/Sidebar/UserSidebar.js
+++ components/Sidebar/UserSidebar.js
@@ -142,7 +142,7 @@ export default function Sidebar(props) {
                     <svg className="MuiSvgIcon-root makeStyles-itemIcon-17" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                       <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
                     </svg>
-                    <div className="MuiListItemText-root makeStyles-itemText-19">Projects</div>
+                    <div className="MuiListItemText-root makeStyles-itemText-19">My Project</div>
                     <span className="MuiTouchRipple-root"></span>
                   </div>
                 </a>
