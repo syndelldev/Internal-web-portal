f61c0f871a2d8f30ca2aa35290e23ca4e743c499
riya patel <riya.syndell@gmail.com>
2022-09-27 16:37:23 +0530

    Update API

--- pages/api/comment/updateComment.js
+++ pages/api/comment/updateComment.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     console.log(req.body)
 
     try{
-        let person=await executeQuery( " update `tbl_comment` set `comment`=? where `id`=? and `username`=? " , [ req.body.comment, req.body.comment_id , req.body.user ] );
+        let person=await executeQuery( "Update `tbl_comment` set `comment`=? where `id`=? and `username`=? " , [ req.body.comment, req.body.comment_id , req.body.user ] );
         res.send(person);
         // console.log(person)
     }
