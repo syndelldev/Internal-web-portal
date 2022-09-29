5618e8db53262b22b456901e5986dabdaae27d1c
priyankap-bit <>
2022-09-29 10:02:07 +0530

    Update

--- components/Sidebar/UserSidebar.js
+++ components/Sidebar/UserSidebar.js
@@ -142,7 +142,7 @@ export default function Sidebar(props) {
                     <svg className="MuiSvgIcon-root makeStyles-itemIcon-17" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
                       <path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
                     </svg>
-                    <div className="MuiListItemText-root makeStyles-itemText-19">Projects</div>
+                    <div className="MuiListItemText-root makeStyles-itemText-19">My Projects</div>
                     <span className="MuiTouchRipple-root"></span>
                   </div>
                 </a>
@@ -152,7 +152,7 @@ export default function Sidebar(props) {
                       <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"></path>
                     </svg>*/}
                     <span className="material-icons MuiIcon-root makeStyles-itemIcon-17  makeStyles-whiteFont-21" aria-hidden="true">content_paste</span>
-                    <div className="MuiListItemText-root makeStyles-itemText-19">Tasks</div>
+                    <div className="MuiListItemText-root makeStyles-itemText-19">My Tasks</div>
                     <span className="MuiTouchRipple-root"></span>
                   </div>
                 </a>
