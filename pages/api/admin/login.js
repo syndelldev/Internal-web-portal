71b4888eb09a3ab65512b4512dce35c6075341ce
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-09-30 15:55:43 +0530

    Update

--- pages/api/admin/login.js
+++ pages/api/admin/login.js
@@ -2,9 +2,11 @@ import nc from "next-connect";
 import { executeQuery } from "../../../config/db";
 const handler=nc();
 
+import bcrypt from 'bcryptjs'
+
 async function login(req,res){
-    var bcrypt = require('bcryptjs');
     
+
     if(req.method === 'POST')
     {
         try{
@@ -13,32 +15,9 @@ async function login(req,res){
             var loginQuery = await executeQuery("select * from tbl_user where email= ? ", [req.body.email] );
             res.status(200).json(loginQuery);
             
-            //console.log(loginQuery)
+            // console.log(loginQuery)
 
             var dbpassword = loginQuery[0].password;
-            
-            // console.log(dbpassword)
-            // console.log(req.body.password)
-
-            bcrypt.compare(dbpassword, req.body.password, function(err, result){
-                console.log("pass", dbpassword)
-                if(dbpassword == req.body.password)
-                {
-                    console.log("Sucess")
-                }
-                else 
-                {
-                    console.log("Fail")
-                }
-            });
-            // if(dbpassword == req.body.password)
-            // {
-            //     console.log("Sucess")
-            // }
-            // else 
-            // {
-            //     console.log("Fail")
-            // }
             
         }
         catch(err){
