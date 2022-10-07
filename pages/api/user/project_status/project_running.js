efc637cedf198b7a8bfef81c3e4165038af30069
riya patel <riya.syndell@gmail.com>
2022-10-07 11:03:38 +0530

    dashboard

--- pages/api/user/project_status/project_running.js
+++ pages/api/user/project_status/project_running.js
@@ -8,9 +8,10 @@ async function projectStatus(req,res) {
     console.log(req.cookies);
 
     try{
-        let projectStatus = await executeQuery("SELECT * FROM `tbl_project` WHERE project_status='running'  AND project_person LIKE ? ", [`%${req.cookies.name}%`]);
+        let projectStatus = await executeQuery("SELECT * FROM `tbl_project` WHERE project_status='running' AND project_person LIKE ? ", [`%${req.cookies.name}%`]);
         res.status(200).json(projectStatus);
-        // console.log(projectStatus)
+        console.log("project");
+        console.log(projectStatus);
     }
     catch(err){
         res.status(500).json(err);
