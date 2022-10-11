dd402a3cd0233f6915aa6a4b27b39f7ff4f08632
ashvinisyndell <106530778+ashvinisyndell@users.noreply.github.com>
2022-10-11 15:08:08 +0530

    Update

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4WIV_ke-vQMk_ImId0UIZpW6pbN2OrKQ",
+    authDomain: "internal-crm-portal.firebaseapp.com",
+    projectId: "internal-crm-portal",
+    storageBucket: "internal-crm-portal.appspot.com",
+    messagingSenderId: "128973785957",
+    appId: "1:128973785957:web:399218b6dc8cea2068af8c"
+};
+
+const app = initializeApp(firebaseConfig);
