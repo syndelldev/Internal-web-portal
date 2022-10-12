705de7949e8cdd47a833cf497c506147f824ad5d
priyankap-bit <>
2022-10-12 15:13:15 +0530

    update config file

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 
 // const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
-// const webpack = require("webpack");
+const webpack = require("webpack");
 const path = require("path");
 
 module.exports =  {
-  // webpack: (config, options) => {
-  //   config.resolve.modules.push(path.resolve("./"));
-  //   return config;
-  // },
+  webpack: (config, options) => {
+    config.resolve.modules.push(path.resolve("./"));
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
