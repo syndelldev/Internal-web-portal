d6da1d4b6d13fa7b0fe7399df95f8ce33bd0d7c5
ashvinisyndell <ashvini.syndell@gmail.com>
2022-10-13 13:52:53 +0530

    update

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://dev-branch.d2t0i33uykx3cf.amplifyapp.com';
+export const server = dev ? 'http://localhost:3000' : 'https://dev-branch.d3qw7nr70bot5z.amplifyapp.com';
