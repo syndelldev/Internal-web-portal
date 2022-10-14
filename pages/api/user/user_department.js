6719beac08443216b20ad4ef42ee389c2089f952
riya patel <riya.syndell@gmail.com>
2022-10-14 16:57:52 +0530

    department table API created

---
+++ pages/api/user/user_department.js
@@ -0,0 +1,21 @@
+import nc from "next-connect";
+import { executeQuery } from "../../../config/db";
+const handler=nc();
+
+
+async function userDepartment(req,res) {
+    
+    console.log(req.cookies);
+
+    try{
+        let userDepartment = await executeQuery("SELECT * FROM `tbl_department` order by `department_name` ");
+        // res.status(200).json(userDepartment);
+        console.log("user task");
+        console.log(userDepartment)
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
+}
+
+export default userDepartment;
