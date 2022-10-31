39dc288c654ef950d7d9408558350e49906f0812
kalpesh-syndell <113024711+kalpesh-syndell@users.noreply.github.com>
2022-10-31 16:59:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,16 +5,22 @@ const { createPool } = require("mysql");
 const { resolve } = require("path");
 
 const pool = createPool({
+    host:"127.0.0.1",
+    user:"root",
+    password:"",
+    port:"3306",
+    database:"web_portal"
+    //============live database connection=================//
     // host:"dbportal.crq074bejaot.ap-northeast-1.rds.amazonaws.com",
     // user:"admin",
     // password:"12345678",
     // port:"3306",
     // database:"dbportal"
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    database: process.env.MYSQL_DATABASE,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
+    // host: process.env.MYSQL_HOST,
+    // port: process.env.MYSQL_PORT,
+    // database: process.env.MYSQL_DATABASE,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASSWORD
 
 })
 
