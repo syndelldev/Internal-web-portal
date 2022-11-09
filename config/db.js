7e209fa11f633549d5e64d0dd3b4ebbdb2425341
priyankap-bit <>
2022-11-09 14:28:19 +0530

    Updated db

--- config/db.js
+++ config/db.js
@@ -11,16 +11,16 @@ const pool = createPool({
     port:"3306",
     database:"web_portal"
     //============live database connection=================//
-    // host:"dbportal.crq074bejaot.ap-northeast-1.rds.amazonaws.com",
-    // user:"admin",
-    // password:"12345678",
-    // port:"3306",
-    // database:"dbportal"
-    // host: process.env.MYSQL_HOST,
-    // port: process.env.MYSQL_PORT,
-    // database: process.env.MYSQL_DATABASE,
-    // user: process.env.MYSQL_USER,
-    // password: process.env.MYSQL_PASSWORD
+    host:"dbportal.crq074bejaot.ap-northeast-1.rds.amazonaws.com",
+    user:"admin",
+    password:"12345678",
+    port:"3306",
+    database:"dbportal"
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
 
 })
 
